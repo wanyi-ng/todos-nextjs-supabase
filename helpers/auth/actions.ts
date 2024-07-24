d5a87f7edca105ead6b57bc24e5263298ebabfbb
wanyi-ng <70354295+wanyi-ng@users.noreply.github.com>
2024-07-24 10:49:05 -0400

    move auth actions to helpers folder

---
+++ helpers/auth/actions.ts
@@ -0,0 +1,46 @@
+"use server"
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { createClient } from "@/utils/supabase/server";
+
+export async function signIn(formData: FormData) {
+  "use server";
+
+  const email = formData.get("email") as string;
+  const password = formData.get("password") as string;
+  const supabase = createClient();
+
+  const { error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  if (error) {
+    return redirect("/login?message=Could not authenticate user");
+  }
+
+  return redirect("/profile");
+};
+
+export async function signUp(formData: FormData) {
+  "use server";
+
+  const origin = headers().get("origin");
+  const email = formData.get("email") as string;
+  const password = formData.get("password") as string;
+  const supabase = createClient();
+
+  const { error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      emailRedirectTo: `${origin}/auth/callback`,
+    },
+  });
+
+  if (error) {
+    return redirect("/login?message=Could not authenticate user");
+  }
+
+  return redirect("/login?message=Check email to continue sign in process");
+};
