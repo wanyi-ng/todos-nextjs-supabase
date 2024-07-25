a4485427fae9ea6b5706859907d2e6ba19e2b98d
wanyi-ng <70354295+wanyi-ng@users.noreply.github.com>
2024-07-25 09:22:18 -0400

    wip /todo page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -4,12 +4,15 @@ import readUserSession from "@/helpers/auth/actions"
 import AuthButton from "@/components/AuthButton"
 import Link from "next/link"
 import Footer from "@/components/Footer"
+import { getAllTodos } from "@/helpers/todo/actions"
 
 
 export default async function ProfilePage() {
   const { data } = await readUserSession()
 
   if (!data.session) return redirect('/login')
+
+  const { data: todos } = await getAllTodos()
 
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
