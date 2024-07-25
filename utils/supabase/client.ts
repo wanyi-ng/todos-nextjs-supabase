f835ab2c4011bcb523a98154b14b239e6928cff1
Wanyi Ng <wanyi.ng1975@gmail.com>
2024-07-24 21:15:16 -0400

    update utility functions to create Supabase clients

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,8 +1,8 @@
-import { createBrowserClient } from "@supabase/ssr";
+import { createBrowserClient } from '@supabase/ssr'
 
 export function createSupabaseBrowserClient() {
-  createBrowserClient(
+  return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  );
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  )
 }
