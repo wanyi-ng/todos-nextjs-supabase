429ea0d6e302fd3c8ee258dfeea503a298e53a7d
Wanyi Ng <wanyi.ng1975@gmail.com>
2024-07-25 20:41:20 -0400

    todo actions

--- helpers/todo/actions.ts
+++ helpers/todo/actions.ts
@@ -11,21 +11,21 @@ export async function addTodo(task: string, user_id: string) {
   const supabase = await createSupabaseBrowserClient()
   const result = await supabase.from("todos").insert({ task, completed: false }).match({ user_id })
 
-  revalidagePath("/todo")
+  revalidatePath("/todo")
   return JSON.stringify(result)
 }
 
 export async function deleteTodoById(id: string) {
   const supabase = await createSupabaseBrowserClient()
-  await supabase.from("todos").delete().eq("id", id) 
-  
+  await supabase.from("todos").delete().eq("id", id)
+
   revalidatePath("/todo")
 }
 
 export async function updateTodoById(id: string, status: boolean) {
   const supabase = await createSupabaseBrowserClient()
   await supabase.from("todos").update({ completed: !status }).eq("id", id)
-  
+
   revalidatePath("/todo")
 }
 
