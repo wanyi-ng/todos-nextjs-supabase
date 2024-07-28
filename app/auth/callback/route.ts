3d88ee7b3dfcce63876ee6904026f5b4e7514715
Wanyi Ng <wanyi.ng1975@gmail.com>
2024-07-28 11:27:20 -0400

    auth: callback and redirect paths based on action

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,29 +1,19 @@
-import { type EmailOtpType } from '@supabase/supabase-js'
 import { type NextRequest, NextResponse } from 'next/server'
-
 import { createSupabaseServerClient } from '@/utils/supabase/server'
-import { redirect } from 'next/navigation'
-import { create } from 'zustand'
 
 export async function GET(request: NextRequest) {
-  const { searchParams } = new URL(request.url)
-  const token_hash = searchParams.get('token_hash')
-  const type = searchParams.get('type') as EmailOtpType | null
-  const next = searchParams.get('next') ?? '/'
+  // The `/auth/callback` route is required for the server-side auth flow implemented
+  // by the SSR package. It exchanges an auth code for the user's session.
+  // https://supabase.com/docs/guides/auth/server-side/nextjs
+  const requestUrl = new URL(request.url)
+  const code = requestUrl.searchParams.get("code")
+  const origin = requestUrl.origin
 
-  if (token_hash && type) {
+  if (code) {
     const supabase = createSupabaseServerClient()
-
-    const { error } = await supabase.auth.verifyOtp({
-      type,
-      token_hash,
-    })
-    if (!error) {
-      // redirect user to specified redirect URL or root of app
-      redirect(next)
-    }
+    await supabase.auth.exchangeCodeForSession(code)
   }
 
-  // redirect the user to an error page with some instructions
-  redirect('/error')
+  // URL to redirect to after sign up process completes
+  return NextResponse.redirect(`${origin}/account`)
 }
--- helpers/auth/actions.ts
+++ helpers/auth/actions.ts
@@ -1,50 +1,58 @@
 "use server"
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
+import { revalidatePath, unstable_noStore as noStore } from "next/cache";
 import { createSupabaseServerClient } from "@/utils/supabase/server";
+
+export async function getUser() {
+  const supabase = createSupabaseServerClient()
+  const { data: { user } } = await supabase.auth.getUser()
+
+  if (!user) return redirect("/login")
+
+  return user
+}
 
 export async function readUserSession() {
   noStore()
-  const supabase = await createSupabaseServerClient()
-  return supabase.auth.getSession()
+  const supabase = createSupabaseServerClient()
+  return await supabase.auth.getSession()
 }
 
 export async function signIn(formData: FormData) {
-  const email = formData.get("email") as string;
-  const password = formData.get("password") as string;
-  const supabase = createSupabaseServerClient();
+  const supabase = createSupabaseServerClient()
 
-  const { error } = await supabase.auth.signInWithPassword({
-    email,
-    password,
-  });
+  const { error } = await supabase.auth.signUp({
+    email: formData.get('email') as string,
+    password: formData.get('password') as string,
+  })
 
   if (error) {
-    return redirect("/login?message=Could not authenticate user");
+    redirect('/login?message=Could not authenticate user')
   }
 
-  return redirect("/todo");
+  revalidatePath('/', 'layout')
+  return redirect('/todo')
 };
 
 export async function signUp(formData: FormData) {
-  const origin = headers().get("origin");
-  const email = formData.get("email") as string;
-  const password = formData.get("password") as string;
-  const supabase = createSupabaseServerClient();
+  const supabase = createSupabaseServerClient()
 
+  const origin = headers().get("origin")
   const { error } = await supabase.auth.signUp({
-    email,
-    password,
+    email: formData.get('email') as string,
+    password: formData.get('password') as string,
     options: {
       emailRedirectTo: `${origin}/auth/callback`,
-    },
-  });
+    }
+  })
 
   if (error) {
-    return redirect("/login?message=Could not authenticate user");
+    redirect('/error')
   }
 
-  return redirect("/login?message=Check email to continue sign in process");
+  revalidatePath('/', 'layout')
+  redirect('/login?message=Check email to continue sign in process')
 };
 
 export async function signOut() {
